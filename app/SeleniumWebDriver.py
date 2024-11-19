4e1654929172e0b72c2fba1a6a38201a91663bd3
mayhem <peon@pop-os.localdomain>
2024-11-18 21:00:40 -0300

    Applied logging lib on the project

--- app/SeleniumWebDriver.py
+++ app/SeleniumWebDriver.py
@@ -5,13 +5,21 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
+import logging
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(filename = 'tesouroDireto.log')
+log.info("SeleniumWeb Start")
 
 class SeleniumWebDriver:
 
     def __init__(self):
 
+        
         self.driver=webdriver.Firefox()
-    
+        log = logging.getLogger(__name__)
+        
 
     def get_webpage_data(self, link):
 
@@ -26,6 +34,7 @@ class SeleniumWebDriver:
         self.soup = BeautifulSoup(self.get_webpage_data(link), 'html.parser')
 
         self.tables = self.soup.find_all('table') # for not duplicating data; more tables were found
+
 
         return self.tables
 
@@ -46,9 +55,11 @@ class SeleniumWebDriver:
                 # regex to clean the data;
                 try:
                     col_set[0] = re.search('[a-zA-Z+\t ]+(\w*)' , col_set[0]).group().strip().replace('\t', '')
-                    print('regex WORKED!')
+                    
                 except Exception as e:
-                    print(f'regex failed in: {col_set}\n{e}')
+                    print(f"""Regex might failed while trying to matching pattern.
+                        \nString value: {col_set}
+                        \n{repr(e)}""")
                     
                 
                 if len(col_set) >= 6: # slicing relevant and irregular data;
@@ -58,6 +69,8 @@ class SeleniumWebDriver:
             
             
             self.dfs.append(pd.DataFrame(row_set))
+        
+        log.info('get_raw_tables runned with sucess')
 
         return self.dfs
             
